4c90eac77ff731f372fda363e6c835f513a5d155
DimaShunko <100087856+DimaShunko@users.noreply.github.com>
2022-06-20 14:22:06 +0300

    Add files via upload

---
+++ FW/FW/src/main/java/example/fw/HelloServlet2.java
@@ -0,0 +1,17 @@
+package example.fw;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloServlet2 extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        response.setContentType("text/html; charset=UTF-8");
+        //2 вариант с jsp
+        request.getRequestDispatcher("/WEB-INF/index.jsp").include(request,response);
+    }
+
+}
---
+++ FW/FW/target/FW-1.0-SNAPSHOT/WEB-INF/index.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Time or date</title>
+</head>
+<body>
+    <%@ page import="example.fw.timeOrDate" %>
+
+    <h1>Время и дата</h1>
+
+    <h2>
+        <%String cmd = request.getParameter("cmd");
+        String com = timeOrDate.commands(cmd);%>
+        <%= com%>
+    </h2>
+
+</body>
+</html>
